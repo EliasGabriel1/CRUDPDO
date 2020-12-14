116cc10f6e36f665ef93a6c0727f043ec3f58b43
EliasGabriel1 <50595684+EliasGabriel1@users.noreply.github.com>
2020-12-13 21:52:20 -0300

    uptade cadastro.php

--- Views/Tela 2/cadastro.php
+++ Views/Tela 2/cadastro.php
@@ -15,12 +15,10 @@
 
     <div class="container">
       <div class="box" style="width: 400px">
-        <header>
-            <div class="logo">
-                <figure>
-                  <img src="https://www.univem.edu.br/img/site/logo.png" width="283.2" height="63.6" alt="logoUnivem">
-                </figure>
-            </div>
+        <div class="logo_isolated">
+          <img class="logo-1" src="https://image.flaticon.com/icons/png/512/702/702814.png" style="height:50px" alt="logo-Univem"> <span class="align-self-end">Inova Ideas</span>
+        </div>
+        <header style="padding-top: 0px;padding-bottom: 0px;">
             <div class="login" style="padding-top: 0px;padding-bottom: 0px;">
               <h1> Crie uma conta</h1>
             </div>
@@ -30,14 +28,14 @@
             <form action="cadastrar.php" method="POST">
               <div class="form-group">
                 <label for="exampleDropdownFormEmail1">Digite seu nome</label>
-                <input type="text" name="nome" class="form-control" id="exampleDropdownFormName" placeholder="João ricardo de sorza" style="width: 383px;">
+                <input type="text" name="nome" class="form-control" id="exampleDropdownFormName" placeholder="JoÃ£o ricardo de sorza" style="width: 383px;">
               </div>
               <div class="form-group">
                 <label for="exampleDropdownFormEmail1">Email address</label>
                 <input type="text" name="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com" style="width: 383px;">
               </div>
               <div class="form-group">
-                <label for="exampleDropdownFormEmail1">RA</label>
+                <label for="exampleDropdownFormEmail1">LOGIN</label>
                 <input type="text" name="usuario" class="form-control" id="ra" placeholder="ex:51237-5" style="width: 383px;">
               </div>
               <div class="form-group">
@@ -48,7 +46,7 @@
                 <label for="exampleDropdownFormPassword1">Confirmar senha</label>
                 <input type="password" name="confirmarsenha" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password" style="width: 383px;">
               </div>
-                <a href="http://localhost/ProjetoScript/Views/ApresentacaoView/apresentacao.php" type="submit" name="cancelar" class="btn btn-light">Cancelar</a>
+                <a href="http://localhost/ProjetoScr1pt/Views/ApresentacaoView/apresentacao.php" type="submit" name="cancelar" class="btn btn-light">Cancelar</a>
                 <a href=""> <button type="submit" class="btn btn-primary">Criar conta</button> </a>              
             </form>
           </div>
